cfe8976da57097fa4c64323f1eb73b88d946821d
Kala Wetzler <kala.wetzler@gmail.com>
2021-02-27 22:07:17 -0500

    Add the start of the advent page -- need links to github

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,7 +6,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: '%s - adventcode',
+    titleTemplate: `kala's adventcode`,
     title: 'adventcode',
     htmlAttrs: {
       lang: 'en'
@@ -21,6 +21,7 @@ export default {
       {rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons'}
     ]
   },
+  extractCSS: true,
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,16 @@
 <template>
-  <div class="d-flex align-center">
+  <div class="d-flex">
     <p>I'm working through a particular advent calendar that involves code & stars.</p>
     <v-icon>auto_awesome</v-icon>
   </div>
 </template>
+
+<script>
+import Day2 from '~/components/advent/Day2.vue'
+
+export default {
+  components: {
+    Day2
+  }
+}
+</script>
