eabe3811c8828baac618d2378b372df86d44049c
Kala Wetzler <kala.wetzler@gmail.com>
2021-02-27 22:07:17 -0500

    Add the start of the advent page -- need links to github

--- pages/advent.vue
+++ pages/advent.vue
@@ -1,55 +1,60 @@
 <template>
-  <v-calendar
-          v-model="value"
-          :weekdays="weekday"
-          type="month"
-          :events="events"
-          event-overlap-mode="stack"
-          :event-overlap-threshold="30"
-          :event-color="getEventColor"
-          @change="getEvents"
-        />
+  <v-data-table
+    :headers="headers"
+    :items="advents"
+    class="elevation-1"
+  >
+  <template v-slot:item.starCount="{ item }">
+    <v-icon v-for="i in item.starCount">star</v-icon>
+  </template>
+  <template v-slot:item.url="{ item }">
+    <a href="url" target="_blank" class="pa-2 text--no-underline">
+      <v-icon height="30" width="30">open_in_new</v-icon>
+    </a>
+  </template>
+</v-data-table>
 </template>
 
 <script>
+import {advents} from '@/data/adventInfo.js';
 export default {
   name: 'advent',
-  data: () => ({
-    type: 'month',
-    weekday: [0, 1, 2, 3, 4, 5, 6],
-    value: '',
-    events: [],
-    colors: ['blue', 'indigo', 'deep-purple', 'cyan', 'green', 'orange', 'grey darken-1'],
-  }),
-  methods: {
-    getEvents ({ start, end }) {
-      const events = []
-
-      const min = new Date(`${start.date}T00:00:00`)
-      const max = new Date(`${end.date}T23:59:59`)
-      const days = (max.getTime() - min.getTime()) / 86400000
-      const eventCount = this.rnd(days, days + 20)
-
-      for (let i = 0; i < eventCount; i++) {
-
-        events.push({
-          name: this.names[this.rnd(0, this.names.length - 1)],
-          color: this.colors[this.rnd(0, this.colors.length - 1)],
-          timed: false,
-        })
-      }
-
-      this.events = events
-    },
-    getEventColor (event) {
-      return event.color
-    },
-    rnd (a, b) {
-      return Math.floor((b - a + 1) * Math.random()) + a
-    },
+  data () {
+    return {
+      headers: [
+        {
+          text: 'Challenge Day',
+          align: 'start',
+          sortable: true,
+          value: 'day',
+        },
+        {
+          text: 'Stars',
+          align: 'start',
+          sortable: true,
+          value: 'starCount',
+        },
+        {
+          text: 'Solution',
+          align: 'start',
+          sortable: true,
+          value: 'url',
+        },
+        {
+          text: 'Tools Used',
+          align: 'start',
+          sortable: true,
+          value: 'tools',
+        },
+      ],
+      advents
+    }
   },
 }
 </script>
 
-<style lang="css" scoped>
+<style lang="scss" scoped>
+  .text--no-underline {
+    text-decoration: none;
+  }
 </style>
